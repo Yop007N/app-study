eb4b88f63ced39ad11bb7eaded6d4628ee52ecd6
enrique <enrique@contac.com>
2023-02-18 18:13:03 -0300

    four commit

--- src/route/users/users.route copy 4.js
+++
@@ -1,9 +0,0 @@
-const userController =require('../../controller/users/users.controller');
-
-module.exports = function(app){
-
-    app.get("/users/list",userController.listar);
-    app.get("/users/update",userController.actualizar);
-    app.get("/users/delete",userController.eliminar);
-
-};
